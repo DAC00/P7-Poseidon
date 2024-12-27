b8bbe6f85a206ac9eddc1eea5af1abe5f30c06ed
DAC00 <boiteaspamv2@gmail.com>
2024-12-27 15:49:18 +0100

    LoginController modification

--- src/main/java/com/opcr/poseidon/controllers/LoginController.java
+++ src/main/java/com/opcr/poseidon/controllers/LoginController.java
@@ -5,11 +5,9 @@ import com.opcr.poseidon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("app")
 public class LoginController {
 
     @Autowired
