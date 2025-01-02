285d36848144c9f526b31c447ae54c65b9e5d30f
DAC00 <boiteaspamv2@gmail.com>
2025-01-02 17:47:29 +0100

    Rating validation, test validation & HTML text -> number

--- src/main/java/com/opcr/poseidon/domain/Rating.java
+++ src/main/java/com/opcr/poseidon/domain/Rating.java
@@ -2,6 +2,7 @@ package com.opcr.poseidon.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
@@ -14,17 +15,17 @@ public class Rating {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @NotBlank(message = "moodysRating is mandatory.")
+    @NotBlank(message = "MoodysRating is mandatory.")
     private String moodysRating;
 
-    @NotBlank(message = "sandPRating is mandatory.")
+    @NotBlank(message = "SandPRating is mandatory.")
     private String sandPRating;
 
-    @NotBlank(message = "fitchRating is mandatory.")
+    @NotBlank(message = "FitchRating is mandatory.")
     private String fitchRating;
 
-    @Positive(message = "orderNumber must be positive.")
-    @NotBlank(message = "orderNumber is mandatory.")
+    @NotNull(message = "Order is mandatory.")
+    @Positive(message = "Order must be positive.")
     private Integer orderNumber;
 
 }
