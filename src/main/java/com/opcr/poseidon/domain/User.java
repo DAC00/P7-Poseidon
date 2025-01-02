d454c60f339f18f51e023f5fdcb28e4261be52dd
DAC00 <boiteaspamv2@gmail.com>
2025-01-02 16:28:31 +0100

    User password regex & validation testing

--- src/main/java/com/opcr/poseidon/domain/User.java
+++ src/main/java/com/opcr/poseidon/domain/User.java
@@ -14,24 +14,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    @NotBlank(message = "Username is mandatory")
+    @NotBlank(message = "Username is mandatory.")
     private String username;
 
-    @NotBlank(message = "Password is mandatory")
+    @NotBlank(message = "Password is mandatory.")
     @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=])(?=.{8,})",
+            regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
             message = "Password needs : 8 characters, " +
                     "one uppercase letter, " +
                     "one lowercase letter, " +
                     "one number " +
-                    "and one special character"
+                    "and one special character."
     )
     private String password;
 
-    @NotBlank(message = "FullName is mandatory")
+    @NotBlank(message = "FullName is mandatory.")
     private String fullname;
 
-    @NotBlank(message = "Role is mandatory")
+    @NotBlank(message = "Role is mandatory.")
     private String role;
 
 }
