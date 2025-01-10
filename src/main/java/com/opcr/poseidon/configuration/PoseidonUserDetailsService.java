139cbe8c5544118183012edc6670bcea97e87e3e
DAC00 <boiteaspamv2@gmail.com>
2025-01-10 13:58:33 +0100

    Comments on UserDetailsService

--- src/main/java/com/opcr/poseidon/configuration/PoseidonUserDetailsService.java
+++ src/main/java/com/opcr/poseidon/configuration/PoseidonUserDetailsService.java
@@ -12,6 +12,13 @@ public class PoseidonUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * Get information on the user with the username if present in database.
+     *
+     * @param username of the user.
+     * @return a UserDetails PoseidonUserSecurity.
+     * @throws UsernameNotFoundException if no user with the username exist.
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository
