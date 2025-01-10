eafa1cc4dc58f231a4dce495a8ff9ab3215acfe7
DAC00 <boiteaspamv2@gmail.com>
2025-01-10 15:28:22 +0100

    Integration Test for bidList

--- src/test/java/com/opcr/poseidon/controllers/BidListControllerTest.java
+++ src/test/java/com/opcr/poseidon/controllers/BidListControllerTest.java
@@ -2,85 +2,95 @@ package com.opcr.poseidon.controllers;
 
 import com.opcr.poseidon.domain.BidList;
 import com.opcr.poseidon.services.BidListService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
-@WebMvcTest(BidListController.class)
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BidListControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @Autowired
     private BidListService bidListService;
 
+    private BidList bidListTest;
+
+    @BeforeEach
+    public void setUp() {
+        this.bidListTest = new BidList();
+        bidListTest.setAccount("test");
+        bidListTest.setType("forTesting");
+        bidListTest.setBidQuantity(91);
+    }
+
     @Test
-    public void validate() throws Exception {
-        doNothing().when(bidListService).saveBidList(any(BidList.class));
+    public void saveBidListTest() throws Exception {
         mockMvc.perform(post("/bidList/validate")
                         .with(csrf())
                         .with(user("user").roles("USER"))
-                        .param("account", "account")
-                        .param("type", "type")
-                        .param("bidQuantity", "10"))
+                        .param("account", bidListTest.getAccount())
+                        .param("type", bidListTest.getType())
+                        .param("bidQuantity", String.valueOf(bidListTest.getBidQuantity())))
                 .andExpect(redirectedUrl("/bidList/list"));
-        verify(bidListService).saveBidList(any(BidList.class));
+
+        List<BidList> list = bidListService.getBidLists();
+        assertEquals(1, list.size());
+        assertTrue(list.stream().anyMatch(
+                bList -> bList.getAccount().equals(bidListTest.getAccount())
+                        && bList.getType().equals(bidListTest.getType())
+                        && bList.getBidQuantity() == bidListTest.getBidQuantity()
+        ));
     }
 
     @Test
-    public void whenAccountIsEmptyShowError() throws Exception {
-        mockMvc.perform(post("/bidList/validate")
+    public void updateBidListTest() throws Exception {
+        bidListService.saveBidList(bidListTest);
+        String updatedType = "updatedType";
+
+        mockMvc.perform(post("/bidList/update/" + bidListTest.getId())
                         .with(csrf())
                         .with(user("user").roles("USER"))
-                        .param("type", "type")
-                        .param("bidQuantity", "10"))
-                .andExpect(content().string(containsString("Account is mandatory.")));
-        verify(bidListService, never()).saveBidList(any(BidList.class));
+                        .param("account", bidListTest.getAccount())
+                        .param("type", updatedType)
+                        .param("bidQuantity", String.valueOf(bidListTest.getBidQuantity())))
+                .andExpect(redirectedUrl("/bidList/list"));
+
+        List<BidList> list = bidListService.getBidLists();
+        assertEquals(1, list.size());
+        assertTrue(list.stream().anyMatch(
+                bList -> bList.getAccount().equals(bidListTest.getAccount())
+                        && bList.getType().equals(updatedType)
+                        && bList.getBidQuantity() == bidListTest.getBidQuantity()
+        ));
     }
 
     @Test
-    public void whenTypeIsEmptyShowError() throws Exception {
-        mockMvc.perform(post("/bidList/validate")
-                        .with(csrf())
-                        .with(user("user").roles("USER"))
-                        .param("account", "account")
-                        .param("bidQuantity", "10"))
-                .andExpect(content().string(containsString("Type is mandatory.")));
-        verify(bidListService, never()).saveBidList(any(BidList.class));
-    }
+    public void deleteBidListTest() throws Exception {
+        bidListService.saveBidList(bidListTest);
 
-    @Test
-    public void whenBidQuantityIsEmptyShowError() throws Exception {
-        mockMvc.perform(post("/bidList/validate")
+        mockMvc.perform(get("/bidList/delete/" + bidListTest.getId())
                         .with(csrf())
-                        .with(user("user").roles("USER"))
-                        .param("account", "account")
-                        .param("type", "type"))
-                .andExpect(content().string(containsString("Bid Quantity must be at least 0.01.")));
-        verify(bidListService, never()).saveBidList(any(BidList.class));
-    }
+                        .with(user("user").roles("USER")))
+                .andExpect(redirectedUrl("/bidList/list"));
 
-    @Test
-    public void whenBidQuantityIsNegativeShowError() throws Exception {
-        mockMvc.perform(post("/bidList/validate")
-                        .with(csrf())
-                        .with(user("user").roles("USER"))
-                        .param("account", "account")
-                        .param("type", "type")
-                        .param("bidQuantity", "-10"))
-                .andExpect(content().string(containsString("Bid Quantity must be at least 0.01.")));
-        verify(bidListService, never()).saveBidList(any(BidList.class));
+        assertEquals(0, bidListService.getBidLists().size());
     }
 }
